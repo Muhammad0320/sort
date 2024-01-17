f32f285452555d8b5923795c20cb6cdcb76b0c22
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-17 05:09:20 +0100

    chore: update number collection

--- src/NumberCollection.ts
+++ src/NumberCollection.ts
@@ -4,4 +4,16 @@ class NumbersCollection {
   get length(): number {
     return this.data.length;
   }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNumber = this.data[leftIndex];
+
+    this.data[leftIndex] = this.data[rightIndex];
+
+    this.data[rightIndex] = leftNumber;
+  }
 }
