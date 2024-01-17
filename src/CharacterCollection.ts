2ba7fd6f2d5190a71c989db4a6f4181d11364aeb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-17 10:33:54 +0100

    chore: update character collection file

---
+++ src/CharacterCollection.ts
@@ -0,0 +1,15 @@
+import { SortableCollection } from "./sorter";
+
+export class CharacterCollection implements SortableCollection {
+  constructor(public data: string) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return (
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
+    );
+  }
+}
