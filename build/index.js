75e4b747c180bcd46c9bcfd86bcc8c588e2002fd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-18 13:52:18 +0100

    chore: update index.ts

--- build/index.js
+++ build/index.js
@@ -1,7 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const sorter_1 = require("./sorter");
 const CharacterCollection_1 = require("./CharacterCollection");
 const charsCollection = new CharacterCollection_1.CharacterCollection("xybxsarup");
-const sorter = new sorter_1.Sorter(charsCollection);
-console.log(sorter.sort().collection);
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,3 @@ import { Sorter } from "./sorter";
 import { CharacterCollection } from "./CharacterCollection";
 
 const charsCollection = new CharacterCollection("xybxsarup");
-
-const sorter = new Sorter(charsCollection);
-
-console.log(sorter.sort().collection);
