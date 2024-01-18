2ef7f6c519ac36147337a0c3666d2da4324d7195
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-01-18 13:44:44 +0100

    chore: update sorter ts file

--- src/sorter.ts
+++ src/sorter.ts
@@ -6,16 +6,20 @@ export interface SortableCollection {
   swap(leftIndex: number, rightIndex: number): void;
 }
 
-export class Sorter {
-  constructor(public collection: SortableCollection) {}
+export abstract class Sorter {
+  abstract compare: (leftIndex: number, rightIndex: number) => boolean;
+
+  abstract length: number;
+
+  abstract swap(leftIndex: number, rightIndex: number): void;
 
   sort() {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
